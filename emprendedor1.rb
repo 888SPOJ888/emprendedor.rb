fc8051ae9224ba0c769e73005af13548752f0b6d
alexis Montealegre <ajmu26@gmail.com>
2020-06-23 15:05:54 -0400

    listo

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -3,14 +3,14 @@
 #gastos = 20000
 
 
-precio_venta=ARGV[0].to_i
+precio_venta=ARGV[0].to_f
 usuarios_basicos=ARGV[1].to_i
-gastos=ARGV[2].to_i
+gastos=ARGV[2].to_f
 
-x = precio_venta * usuarios_basicos - gastos
+utilidades = (precio_venta * usuarios_basicos) - gastos
 
-if x > 0
-    puts "utilidades son de #{x * 0.35}"
+if utilidades > 0
+    puts utilidades - (utilidades*0.35)
 else
-    puts "las utilidades son menores o igual a 0, por lo que sus utilidades son #{x}" 
+    puts utilidades
 end
--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,14 +1,14 @@
-precio_venta=ARGV[0].to_i
+precio_venta=ARGV[0].to_f
 usuarios_basico=ARGV[1].to_i
 usuarios_premium=ARGV[2].to_i
-usuarios_gratuitos=ARGV[3].to_i
+usuarios_gratuitos=ARGV[3].to_f
 gastos=ARGV[4].to_i
 
-x = (precio_venta*usuarios_basico)+(precio_venta + 2 * usuarios_premium)-gastos.to_f
+utilidades = (precio_venta*(usuarios_premium*(precio_venta*2)))-gastos
 
-if x > 0
-    puts "utilidades son de #{x * 0.65}"
+if utilidades > 0
+    puts utilidades - (utilidades * 0.35)
 else
-    puts "las utilidades son menores o igual a 0, por lo que sus utilidades son #{x}" 
+    puts utilidades
 end
 
