dcd284233c8cb569ead2d5c8c5a09119587b2e65
alexis Montealegre <ajmu26@gmail.com>
2020-06-23 22:45:38 -0400

    ready

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -4,13 +4,14 @@
 
 
 precio_venta=ARGV[0].to_f
-usuarios_basicos=ARGV[1].to_i
-gastos=ARGV[2].to_f
+usuarios_basicos=ARGV[1].to_f
+gastos=ARGV[2].to_i
 
-utilidades = (precio_venta * usuarios_basicos) - gastos
+utilidades = (((precio_venta)*usuarios_basicos)-gastos)
 
 if utilidades > 0
-    puts utilidades - (utilidades*0.35)
+    impuestos = (utilidades*0.35)
+    puts "Con los datos ingresados generamos #{utilidades}, por lo tanto, es rentable y debe pagar #{impuestos} pesos de impuestos"
 else
-    puts utilidades
+    puts "Con los datos ingresados generamos #{utilidades}, por lo tanto, no es rentable y al ser negativo no debe pagar impuestos"
 end
--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,14 +1,14 @@
-precio_venta=ARGV[0].to_f
-usuarios_basico=ARGV[1].to_i
-usuarios_premium=ARGV[2].to_i
-usuarios_gratuitos=ARGV[3].to_f
-gastos=ARGV[4].to_i
+precio_venta=ARGV[0].to_i
+usuarios_basico=ARGV[1].to_f
+usuarios_premium=ARGV[2].to_f
+usuarios_gratuitos=ARGV[3].to_i
+gastos=ARGV[4].to_f
 
-utilidades = (precio_venta*(usuarios_premium*(precio_venta*2)))-gastos
+utilidades = (((usuarios_basicos*precio_venta*(1))+(usuarios_premium*precio_venta*(2))-(usuarios_gratuitos*precio_venta*(1)))- gastos)
 
 if utilidades > 0
-    puts utilidades - (utilidades * 0.35)
+    impuestos=(utilidades * 0.35)
+    puts "Con los datos ingresados generamos #{utilidades}, por lo tanto, es rentable y debe pagar #{impuestos} pesos de impuestos"
 else
-    puts utilidades
+    puts "Con los datos ingresados generamos #{utilidades}, por lo tanto, no es rentable y al ser negativo no debe pagar impuestos"
 end
-
